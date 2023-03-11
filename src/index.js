e296be498341db63e47681ee7b56d02f939dd8ba
Skripnuyk <walker8613@gmail.com>
2023-03-11 15:36:29 +0200

    update styles.css, fixed fetchCountries.js and added  task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { countryCardTemplate, countryListTemplate } from './js/country-template';
+import { refs } from './js/refs';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+refs.searchBox.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
+
+function onInputCountry(event) {
+    event.preventDefault();
+    let countryName = refs.searchBox.value;
+    if (countryName.trim() === '') {
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+    }
+
+    fetchCountries(countryName.trim())
+        .then(countries => {
+            if (countries.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name.');
+                refs.countryInfo.innerHTML = '';
+                refs.countryList.innerHTML = '';
+                return;
+            }
+            
+            if (countries.length > 1 && countries.length <= 10) {
+                const listMarkup = countries.map(country => countryListTemplate(country));
+                refs.countryList.innerHTML = listMarkup.join('');
+                refs.countryInfo.innerHTML = '';
+                }
+            
+            if (countries.length === 1) {
+                const infoMarkup = countries.map(country => countryCardTemplate(country));
+                refs.countryInfo.innerHTML = infoMarkup.join('');
+                refs.countryList.innerHTML = '';
+                }
+    })
+        .catch(error => {
+            Notify.failure('Oops, there is no country with that name')
+            refs.countryInfo.innerHTML = '';
+            refs.countryList.innerHTML = '';
+            return error;
+});
+};
--- src/js/country-template.js
+++ src/js/country-template.js
@@ -6,8 +6,8 @@ export function countryCardTemplate({ name, capital, flags, population, language
     <h2 class="country-info__name">${name.official}</h2>
     </div>
     <p class="country-info__capital"><span class="country-info__weight">Capital:</span>${capital}</p>
-    <p class="country-info__capital"><span class="country-info__weight">Population:</span>${population}</p>
-    <p class="country-info__capital"><span class="country-info__weight">Languages:</span>${Object.values(languages)}</p>
+    <p class="country-info__population"><span class="country-info__weight">Population:</span>${population}</p>
+    <p class="country-info__languages"><span class="country-info__weight">Languages:</span>${Object.values(languages)}</p>
     </div>`;
 }
 
