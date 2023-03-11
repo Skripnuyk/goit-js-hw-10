d5b8a960fc74b2a9ba3d34316e2aa00b9aa8317b
Skripnuyk <walker8613@gmail.com>
2023-03-11 13:00:21 +0200

    added country-template.js and refs.js

---
+++ src/js/country-template.js
@@ -0,0 +1,19 @@
+export function countryCardTemplate({ name, capital, flags, population, languages }) {
+    return `<div class="country-info__container">
+    <div class="country-info__wrap">
+    <img class="country-info__flags" src="${flags.svg}" alt="${name.official}"
+    width="50" />
+    <h2 class="country-info__name">${name.official}</h2>
+    </div>
+    <p class="country-info__capital"><span class="country-info__weight">Capital:</span>${capital}</p>
+    <p class="country-info__capital"><span class="country-info__weight">Population:</span>${population}</p>
+    <p class="country-info__capital"><span class="country-info__weight">Languages:</span>${Object.values(languages)}</p>
+    </div>`;
+}
+
+export function countryListTemplate({ name, flags }) {
+    return `<li class="country-list__item">
+    <img class="country-list__img" src="${flags.svg}" alt="${name.official}"
+    width="25" />
+    <h2 class="country-list__name">${name.official}</h2></li>`;
+}
